891e5f11f3861f8bdc2df2f1353744feaa1dd566
Parasv17 <87497954+Parasv17@users.noreply.github.com>
2023-07-03 13:44:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import Navbar_2 from './components/Navbar_2';
+import AddProf from './components/AddProf';
+import AddSchool from './components/AddSchool';
 // import '../App.css';
 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
+     <Routes>
+        <Route path="/" element={<><Navbar/><Home/></>} />
+        <Route path="/addprof" element={<><Navbar_2/><AddProf /></>} />
+        <Route path="/addschool" element={<><Navbar_2/><AddSchool /></>} />
+        <Route path="/signup" element={<><Navbar/><SignUp/></>} />
+        <Route path="/login" element={<><Navbar/><Login/></>} />
       </Routes>
     </>
   );
